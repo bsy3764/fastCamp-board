bd6ea742c1a6fdb8fd658cc353cab061dab12d76
bang <bsy3764@gmail.com>
2025-09-13 23:25:09 +0900

    #6 data-rest 설정 및 rest api 간략 테스트

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource // 도메인 모델과 repository를 분석해서, RESTful API를 제공 -> 실헹해서 http://localhost:8080/api 확인해보기(spring-data-rest-hal-explorer)
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
---
+++ src/test/java/com/example/demo/controller/DataRestTest.java
@@ -0,0 +1,73 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@DisplayName("spring data rest api 테스트")    // api 존재 여부만 빠르게 확인
+@Transactional  // 테스트 끝나고 롤백
+@AutoConfigureMockMvc   // @SpringBootTest 만으론 MockMvc 를 인지 못해서 추가해줌
+@SpringBootTest // 통합 테스트
+// @WebMvcTest // MockMvc 를 사용할 수 있게 해줌, 슬라이스 테스트, 컨트롤러 빈 외의 빈들을 로드하지 않음
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("게시글 리스트 조회")
+    @Test
+    void articleListSelect() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+                //.andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("게시글 단건 조회")
+    @Test
+    void articleSelect() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+        //.andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("게시글에 달린 댓글 리스트 조회")
+    @Test
+    void commentListFromArticleSelect() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+        //.andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("댓글 리스트 조회")
+    @Test
+    void commentListSelect() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+        //.andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("댓글 단건 조회")
+    @Test
+    void commentSelect() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+        //.andDo(MockMvcResultHandlers.print());
+    }
+}
