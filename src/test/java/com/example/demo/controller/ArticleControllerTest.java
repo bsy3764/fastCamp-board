a03f3fd083b53dbdbde20632eb3a2509054896dd
bang <bsy3764@gmail.com>
2025-09-14 13:51:04 +0900

    #10 타임리프 설정 및 뷰 테스트 작성

---
+++ src/test/java/com/example/demo/controller/ArticleControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("뷰 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) // 지정한 컨트롤러만 로딩해서 테스트
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("게시판 리스트 페이지")
+    @Test
+    void getMethodArticleListTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+    }
+
+    @DisplayName("게시글 페이지")
+    @Test
+    void getMethodArticleTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("article"));
+    }
+
+    @DisplayName("게시글 검색 전용 페이지")
+    @Test
+    void getMethodArticleSearchTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("게시글 해시태그 검색 페이지")
+    @Test
+    void getMethodArticleHashtagSearchTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+    }
+
+}
